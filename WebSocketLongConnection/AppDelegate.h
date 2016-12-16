93f14ea0bc658bcf8d4468ff127f59fc2dd2dadb
ziv ng <ziv_ng@163.com>
2016-12-16 23:55:33 +0800

    Initial Commit

---
+++ WebSocketLongConnection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebSocketLongConnection
+//
+//  Created by ziv on 2016/12/16.
+//  Copyright © 2016年 ziv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
